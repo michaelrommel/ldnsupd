f5a222468f9b043d03ee884e1e35a91488e12ecb
Michael Rommel <rommel@layer-7.net>
2026-02-12 21:45:07 +0100

    feat: first attempt

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/michaelrommel/ldnsupd
+
+go 1.24.6
+
+require github.com/libdns/libdns v1.0.0
---
+++ provider.go
@@ -0,0 +1,92 @@
+// Package libdnstemplate implements a DNS record management client compatible
+// with the libdns interfaces for <PROVIDER NAME>. TODO: This package is a
+// template only. Customize all godocs for actual implementation.
+package ldnsupd
+
+import (
+	"context"
+	"fmt"
+	"sync"
+
+	"github.com/libdns/libdns"
+)
+
+// TODO: Providers must not require additional provisioning steps by the callers; it
+// should work simply by populating a struct and calling methods on it. If your DNS
+// service requires long-lived state or some extra provisioning step, do it implicitly
+// when methods are called; sync.Once can help with this, and/or you can use a
+// sync.(RW)Mutex in your Provider struct to synchronize implicit provisioning.
+
+// Provider facilitates DNS record manipulation with <TODO: PROVIDER NAME>.
+type Provider struct {
+	// TODO: Put config fields here (with snake_case json struct tags on exported fields), for example:
+	TSIGKeyName string `json:"tsig_key_name,omitempty"`
+	TSIGSecret string `json:"tsig_secret,omitempty"`
+	DNSServer string `json:"dns_server,omitempty"`
+	mutex sync.Mutex
+}
+
+// GetRecords lists all the records in the zone.
+func (p *Provider) GetRecords(ctx context.Context, zone string) ([]libdns.Record, error) {
+	// Make sure to return RR-type-specific structs, not libdns.RR structs.
+	libRecords, err := p.getDomain(ctx, zone)
+	if err != nil {
+		return nil, err
+	}
+	return libRecords, nil
+}
+
+// AppendRecords adds records to the zone. It returns the records that were added.
+func (p *Provider) AppendRecords(ctx context.Context, zone string, records []libdns.Record) ([]libdns.Record, error) {
+	// Make sure to return RR-type-specific structs, not libdns.RR structs.
+	var appendedRecords []libdns.Record
+
+	for _, rec := range records {
+		err := p.updateRecord(ctx, zone, rec, false)
+		if err != nil {
+			return nil, err
+		}
+		appendedRecords = append(appendedRecords, rec)
+	}
+
+	return appendedRecords, nil
+}
+
+// SetRecords sets the records in the zone, either by updating existing records or creating new ones.
+// It returns the updated records.
+func (p *Provider) SetRecords(ctx context.Context, zone string, records []libdns.Record) ([]libdns.Record, error) {
+	var setRecords []libdns.Record
+
+	for _, rec := range records {
+		err := p.updateRecord(ctx, zone, rec, false)
+		if err != nil {
+			return nil, err
+		}
+		setRecords = append(setRecords, rec)
+	}
+
+	return setRecords, nil
+}
+
+// DeleteRecords deletes the specified records from the zone. It returns the records that were deleted.
+func (p *Provider) DeleteRecords(ctx context.Context, zone string, records []libdns.Record) ([]libdns.Record, error) {
+	var deletedRecords []libdns.Record
+
+	for _, rec := range records {
+		err := p.updateRecord(ctx, zone, rec, true)
+		if err != nil {
+			return nil, err
+		}
+		deletedRecords = append(deletedRecords, rec)
+	}
+
+	return deletedRecords, nil
+}
+
+// Interface guards
+var (
+	_ libdns.RecordGetter   = (*Provider)(nil)
+	_ libdns.RecordAppender = (*Provider)(nil)
+	_ libdns.RecordSetter   = (*Provider)(nil)
+	_ libdns.RecordDeleter  = (*Provider)(nil)
+)
